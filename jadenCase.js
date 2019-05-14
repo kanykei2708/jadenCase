50b47ed751f7378ae9f4db14bf3cbef3f1b0557d
kanykei2708 <48042863+kanykei2708@users.noreply.github.com>
2019-05-13 22:43:55 -0400

    Update jadenCase.js

--- jadenCase.js
+++ jadenCase.js
@@ -1,20 +1,18 @@
-
-function jadenCase(text) {
+function jadenCase() {
   var text = document.getElementById('original').value;
 
-  //getting the text from the input field, set it equal to variable text
+  var newText = text.split(" ");
+  
+  firstLetters = newText.map( val => {
+     return val[0].toUpperCase() + val.slice(1);
+   }
+   );
+   
+  newText = firstLetters.join(" ");
+   
+  document.getElementById("editedText").innerHTML = newText;
+  document.getElementById("editedText").textContent = newText;
 
-    var newText = {
-      //create a variable newText.  newText is equal to the result of calling the split method on text, and calling the map
-      // method on the resulting array
+}
 
-      //return the first letter of each element in the array (x[0])
-      //concatenate it with the rest of the letters in each element (x.slice(1))
 
-    //turn the array back into a sentence
-
-    // document.getElementById("editedText").innerHTML = newText;
-    document.getElementById("editedText").textContent = newText;
-
-    //insert the newText into the document
-};
